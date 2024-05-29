f6661c26c64b54fccdd1154296cf765aee2325e2
Ncir Jaweher <jaweherncir@gmail.com>
2024-05-29 06:29:50 +0100

    nodejs mysql  react js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import DashboardAdmin from './dashboardadmin/Dashboardadmin';
+import DemandeFinForm from './dashboardclient/DemandeFinForm.js';
+import DashboardClient from './dashboardclient/dashboardclient';
+import ForgotPassword from './elements/ForgotPassword';
+import Login from './elements/Login';
+import Register from './elements/Register';
+import Home from './home/Home';
+import Dashboard from './home/dashboard';
+import GestionOpportunites from './dashboardadmin/GestionOpportunites';
+import Profile from './dashboardclient/profile.js';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboardadmin" element={<DashboardAdmin />} />
+          <Route path="/dashboardclient" element={<DashboardClient/>} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/demendesfin" element={<DemandeFinForm />} />
+          <Route path="/gestion-opportunites" element={<GestionOpportunites />} />
+          <Route path="/profile" element={<Profile />} />
+
+
+        </Routes>
+    </Router>
   );
 }
 
